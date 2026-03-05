fddddb92fbf6ea7d5db304337128d92f84a2ea3f
james <159420270+Beamzi@users.noreply.github.com>
2026-03-05 21:08:03 +1100

    moved button-gradient to sryles dir

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Sulphur_Point } from "next/font/google";
 import "./globals.css";
-import "./button-gradient.css";
+import "../styles/button-gradient.css";
 import Navbar from "@/components/Navbar";
 import FooterConditional from "@/components/FooterConditional";
 
